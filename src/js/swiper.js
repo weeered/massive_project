90d3f2e09a851a170a520d9832aa851ffd9a7839
weeered <qturtle22@g,ail.com>
2023-12-01 21:19:11 +0300

    update 1

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,9 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js";
+  import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
   loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
 
   // And if we need scrollbar
   scrollbar: {
